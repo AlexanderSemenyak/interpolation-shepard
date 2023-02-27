bf0d5d89ee41f13dc0e0830cbecbd71addd0abd3
wrathchild14 <jovanprodanov123@gmail.com>
2023-02-27 23:18:08 +0100

    Init repo

---
+++ Point.cs
@@ -0,0 +1,21 @@
+﻿namespace InterpolationShepard
+{
+    internal class Point
+    {
+        public Point(float v1, float v2, float v3, float v4)
+        {
+            X = v1;
+            Y = v2;
+            Z = v3;
+            Value = v4;
+        }
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public float Value { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace InterpolationShepard;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        // this can stay this way
+        // Console.WriteLine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location));
+        var filePath = Path.Combine(@"C:\Git\aot\InterpolationShepard\data\point-cloud-1k.raw");
+
+        var binReader = new BinaryReader(File.Open(filePath, FileMode.Open));
+
+        var dataPoints = binReader.ReadInt32();
+
+        for (int i = 0; i < dataPoints; i++)
+        {
+            var point = new Point(binReader.ReadSingle(), binReader.ReadSingle(), binReader.ReadSingle(), binReader.ReadSingle());
+            Console.WriteLine(point.X + " " + point.Y + " " + point.Z + " " + point.Value);
+        }
+    }
+}
