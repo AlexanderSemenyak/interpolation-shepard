6fc9cc4d24d75019682f06f2e173ebc96f3e0a3d
wrathchild14 <jovanprodanov123@gmail.com>
2023-03-03 20:26:11 +0100

    Reformat code

--- Program.cs
+++ Program.cs
@@ -2,28 +2,24 @@
 
 internal static class Program
 {
+    private static bool IsLinux
+    {
+        get
+        {
+            var p = (int)Environment.OSVersion.Platform;
+            return p == 4 || p == 6 || p == 128;
+        }
+    }
+
     private static void Main(string[] args)
     {
-        string? projectDirectory;
-        if (IsLinux)
-            projectDirectory = Environment.CurrentDirectory;
-        else
-            projectDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
+        var projectDirectory = IsLinux ? Environment.CurrentDirectory : Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
 
         var filePath = Path.Combine(projectDirectory + "/data/point-cloud-10k.raw");
 
         var shepardInterpolation = new ShepardInterpolation();
         shepardInterpolation.LoadData(filePath);
-        shepardInterpolation.InitializeCubeVolume(16, 8, 16);
+        shepardInterpolation.InitializeVolume(16, 16, 8);
         shepardInterpolation.InterpolateToFile("output.ppm");
-    }
-
-    public static bool IsLinux
-    {
-        get
-        {
-            int p = (int)Environment.OSVersion.Platform;
-            return (p == 4) || (p == 6) || (p == 128);
-        }
     }
 }
